ad20f9b21b677868e270a8b4b7917f28de71bc90
laogui <laogui@laogui.local>
2017-05-10 11:48:52 +0800

    update server

--- server.php
+++ server.php
@@ -8,11 +8,41 @@
 
 require dirname(__FILE__)."/vendor/autoload.php";
 
+
+
+$router = new \Sukui\Router();
+
+$router->get("/user/{id:\d+}",function (\Sukui\ServerContext $context,$next,$vars){
+        $context->body = "user={$vars['id']}";
+});
+
+
+$router->addRoute(["GET","POST"],"/test",function (\Sukui\ServerContext $context,$next,$vars){
+    $context->body = "test";
+});
+
+$router->get("/timeout",function (\Sukui\ServerContext $context,$next,$vars){
+    yield async_sleep(1000);
+});
+
+
+$router->addGroup("/admin",function(\FastRoute\RouteCollector $router){
+    $router->addRoute("GET","/test1",function (\Sukui\ServerContext $context,$next,$vars){
+        $context->body = "admin/test1";
+    });
+
+    $router->addRoute("GET","/test2",function (\Sukui\ServerContext $context,$next,$vars){
+        $context->body = "admin/test2 with ".json_encode($vars);
+    });
+});
+
+
 $app = new \Sukui\Application();
 
-$app->addMiddleware(function(\Sukui\ServerContext $ctx) {
-    $ctx->status = 200;
-    $ctx->body = "<h1>Hello World</h1>";
-});
+$app->addMiddleware(new \Sukui\Middleware\RequestTimeout(200));
+
+$app->addMiddleware($router->routes());
+
+$app->addMiddleware(new \Sukui\Middleware\NotFound());
 
 $app->listen();
